75d215f2cb46c2889a01d25c105a44d02f2c72f4
sinbadkhang <singbad1998@gmail.com>
2018-12-30 18:46:16 +0700

    Add these pages: index product, about us, contact

---
+++ DienMayQT/Areas/Customer/Controllers/ProductsCustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DienMayQT.Models;
+
+namespace DienMayQT.Areas.Customer.Controllers
+{
+    public class ProductsCustomerController : Controller
+    {
+        private DmQT06Entities1 db = new DmQT06Entities1();
+
+        // GET: Customer/ProductsCustomer
+        public ActionResult Index()
+        {
+            var model = new KhuModels();
+            model.Product = db.Product.ToList();
+            model.ProductType = db.ProductType.ToList();
+            return View(model);
+        }
+
+        // GET: /BangSanPham/Details/5
+        public FileResult GetImgSrc(int id)
+        {
+            var path = Server.MapPath("~/App_Data/" + id);
+            return File(path, "images");
+        }
+
+        // GET: Customer/ProductsCustomer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
---
+++ DienMayQT/Models/KhuModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DienMayQT.Models
+{
+    public class KhuModels
+    {
+        public IEnumerable<Product> Product { get; set; }
+        public IEnumerable<ProductType> ProductType { get; set; }
+        
+    }
+}
